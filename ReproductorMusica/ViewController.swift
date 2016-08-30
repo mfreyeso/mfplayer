5a3ab9513ebfff2e4c5488aa58879d1b9f069a36
Mario Reyes Ojeda <mfreyesojeda@gmail.com>
2016-08-29 19:21:30 -0500

    Add layout and connect widgets with Interface Builder and ViewController

--- ReproductorMusica/ViewController.swift
+++ ReproductorMusica/ViewController.swift
@@ -7,17 +7,65 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    let songs = ["Ella", "Bajito", "Otra vez", "Australia", "Cold Water", "Island in the Sun", "Quemando Amor"]
+    var songItem :String?
+    
+    
+    
+    @IBOutlet weak var songImage: UIImageView!
+    @IBOutlet weak var songTitle: UILabel!
+    @IBOutlet weak var songState: UILabel!
+    
+    @IBOutlet weak var songSelector: UIPickerView!
+    
+    @IBAction func playButton() {
+        songTitle.text = songItem!
+        songState.text = "Now Playing"
+    }
+    
+    @IBAction func pauseButton() {
+        songTitle.text = songItem!
+        songState.text = "Paused"
+    }
+    
+    @IBAction func stopButton() {
+        songTitle.text = songItem!
+        songState.text = "Stopped"
+    }
+    
+    
+    @IBOutlet weak var optionShuffle: UILabel!
+    
+    @IBOutlet weak var sliderVolume: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        songSelector.delegate = self
+        songSelector.dataSource = self
+        songSelector.selectRow(1, inComponent: 0, animated: false)
+        songItem = songs[1]
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return songs.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return songs[row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        songItem = songs[songSelector.selectedRowInComponent(0)]
     }
 
 
